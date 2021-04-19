3e9263e43606afa66aaa9d422db97c5b235a89a5
NKaty <nd.katy@gmail.com>
2021-04-19 16:27:03 +0300

    refactor authentication logic

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Browser, Page, Protocol } from 'puppeteer';
+import puppeteer, { Browser, Page } from 'puppeteer';
 import { Writable } from 'stream';
 import { createWriteStream } from 'fs';
 import { resolve } from 'path';
@@ -6,23 +6,29 @@ import ratelimit from 'promise-ratelimit';
 import dotenv from 'dotenv';
 import toISOStringTimezoneOffset from './utils/convertDate';
 import { Nullable, Advert } from './types';
-import Cookie = Protocol.Network.Cookie;
 
 dotenv.config();
 
 class Scraper {
   private browser: Nullable<Browser> = null;
-  private cookies: Nullable<Array<Cookie>> = null;
   private outputStream: Nullable<Writable> = null;
-  private authPage: Nullable<Page> = null;
+  private requestPage: Nullable<Page> = null;
   private firstAdvert = true;
-  private cookieError = false;
+  private auth = false;
   // To make pauses between requests
   private throttle = ratelimit(3000);
   // Key for avito internal api
   private key = 'af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir';
   private userAgent =
     'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36';
+
+  private beforeStart(auth: boolean) {
+    this.browser = null;
+    this.outputStream = null;
+    this.requestPage = null;
+    this.firstAdvert = true;
+    this.auth = auth;
+  }
 
   // Creates a browser instance
   private async startBrowser(): Promise<void> {
@@ -94,39 +100,45 @@ class Scraper {
     }
   }
 
-  // Gets authentication cookie
+  // Gets a mobile page to make request
+  // The page can be with an authentication cookie or not
+  // Authentication could be needed to get a phone number
   // Whether you can get a phone number without authentication or
   // not depends on the settings of the phone number owner
   // So in order to obtain all phone numbers you have to log in to avito
+  // ATTENTION:
   // But if you scrape phone numbers while logged in,
   // your avito account will be banned after about 20 phone numbers
   // So this code is written for educational purposes only and I don't
   // recommend using it in real scraping
-  private async getCookie(): Promise<void> {
+  private async getRequestPage(): Promise<void> {
     if (!this.browser) return;
 
     try {
-      this.authPage = await this.browser.newPage();
+      this.requestPage = await this.browser.newPage();
       // Increase the maximum navigation time
       // Sometimes 30 seconds is not enough
-      await this.authPage.setDefaultTimeout(120000);
+      await this.requestPage.setDefaultTimeout(120000);
       // For a mobile version
-      await this.authPage.setUserAgent(this.userAgent);
-      await this.authPage.goto('https://m.avito.ru/#login');
-      await this.authPage.waitForSelector('#app');
+      await this.requestPage.setUserAgent(this.userAgent);
+      await this.requestPage.goto('https://m.avito.ru/#login');
+      await this.requestPage.waitForSelector('#app');
 
       // Username and password are stored in environment variables (.env file)
       // Remember: your avito account will be banned after 20 phone numbers
-      if (process.env.USERNAME && process.env.PASSWORD) {
+      if (this.auth && process.env.USERNAME && process.env.PASSWORD) {
         // Puppeteer recommends this pattern for click, but sometimes
         // it doesn't work (TimeoutError: Navigation timeout exceeded)
         // await Promise.all([
         //   page.waitForNavigation(),
         //   page.click('div[data-marker="login-button"'),
         // ]);
-        await this.authPage.click('div[data-marker="login-button"');
-        await this.authPage.type('input[name="login"]', process.env.USERNAME);
-        await this.authPage.type(
+        await this.requestPage.click('div[data-marker="login-button"');
+        await this.requestPage.type(
+          'input[name="login"]',
+          process.env.USERNAME
+        );
+        await this.requestPage.type(
           'input[name="password"]',
           process.env.PASSWORD
         );
@@ -136,14 +148,10 @@ class Scraper {
         //   page.waitForNavigation(),
         //   page.click('input[type="submit"'),
         // ]);
-        await this.authPage.click('input[type="submit"');
+        await this.requestPage.click('input[type="submit"');
       }
-      this.cookies = await this.authPage.cookies();
     } catch (err) {
       console.log('Could not get cookie: ', err);
-      // If we have an error while getting cookies,
-      // we don't try to get them every time we make request to the api
-      this.cookieError = true;
     }
   }
 
@@ -151,62 +159,61 @@ class Scraper {
   private async getAdvertData(id: string): Promise<Nullable<Advert>> {
     let advert: Nullable<Advert> = null;
     if (!this.browser) return advert;
-    let page: Page | undefined;
 
     try {
-      if (!this.cookieError && !this.cookies) await this.getCookie();
-      const page = await this.browser.newPage();
-      // For a mobile version
-      await page.setUserAgent(this.userAgent);
-      if (this.cookies) await page.setCookie(...this.cookies);
+      if (this.requestPage) {
+        // Make a pause before the request to avoid being banned
+        // for too frequent access to the internal api
+        await this.throttle();
+        // Get all the information except the phone number
+        const advertResponse = await this.requestPage.goto(
+          `https://m.avito.ru/api/15/items/${id}?key=${this.key}`
+        );
+        const advertData = await advertResponse.json();
 
-      // Make a pause before the request to avoid being banned
-      // for too frequent access to the internal api
-      await this.throttle();
-      // Get all the information except the phone number
-      const advertResponse = await page.goto(
-        `https://m.avito.ru/api/15/items/${id}?key=${this.key}`
-      );
-      const advertData = await advertResponse.json();
+        // Make a pause before the request to avoid being banned
+        // for too frequent access to the internal api
+        // Although if you scrape phone numbers while logged in,
+        // your avito account will still be banned after about 20 phone numbers
+        await this.throttle();
+        // Get the phone number
+        // We don't check here to see if we have the authentication cookie,
+        // because we can get some phone numbers even without authentication
+        const phoneResponse = await this.requestPage.goto(
+          `https://m.avito.ru/api/1/items/${id}/phone?key=${this.key}`
+        );
+        const phoneData = await phoneResponse.json();
+        const phone = phoneData.result?.action?.uri?.split('%2B').slice(-1)[0];
+        console.log(phoneData);
 
-      // Make a pause before the request to avoid being banned
-      // for too frequent access to the internal api
-      // Although if you scrape phone numbers while logged in,
-      // your avito account will still be banned after about 20 phone numbers
-      await this.throttle();
-      // Get the phone number
-      // We don't check here to see if we have the authentication cookie,
-      // because we can get some phone numbers even without authentication
-      const phoneResponse = await page.goto(
-        `https://m.avito.ru/api/1/items/${id}/phone?key=${this.key}`
-      );
-      const phoneData = await phoneResponse.json();
-      const phone = phoneData.result?.action?.uri?.split('%2B').slice(-1)[0];
-
-      advert = {
-        title: advertData.title || '',
-        description: advertData.description || '',
-        url: advertData.seo?.canonicalUrl || '',
-        price: isNaN(+advertData.firebaseParams?.itemPrice)
-          ? 0
-          : +advertData.firebaseParams.itemPrice,
-        author: advertData.seller?.name || '',
-        date: isNaN(+advertData.time)
-          ? ''
-          : toISOStringTimezoneOffset(+advertData.time),
-        phone: isNaN(+phone) ? '' : phone,
-      };
+        advert = {
+          title: advertData.title || '',
+          description: advertData.description || '',
+          url: advertData.seo?.canonicalUrl || '',
+          price: isNaN(+advertData.firebaseParams?.itemPrice)
+            ? 0
+            : +advertData.firebaseParams.itemPrice,
+          author: advertData.seller?.name || '',
+          date: isNaN(+advertData.time)
+            ? ''
+            : toISOStringTimezoneOffset(+advertData.time),
+          phone: isNaN(+phone) ? '' : phone,
+        };
+      }
     } catch (err) {
       console.log(`Could not get advert (${id}): `, err);
     }
 
-    if (page && !page.isClosed()) await page.close();
     return advert;
   }
 
   // Writes the adverts to the file
   private async getAdverts(ids: string[]): Promise<void> {
     try {
+      if (!this.requestPage || this.requestPage.isClosed()) {
+        await this.getRequestPage();
+      }
+
       for (const id of ids) {
         const advert: Nullable<Advert> = await this.getAdvertData(id);
         if (this.outputStream && advert) {
@@ -259,14 +266,13 @@ class Scraper {
   // Cleans after scraping
   private async clean(): Promise<void> {
     this.firstAdvert = true;
-    this.cookieError = false;
-    this.cookies = null;
+    this.auth = false;
 
     try {
-      if (this.authPage && !this.authPage.isClosed()) {
-        await this.authPage.close();
+      if (this.requestPage && !this.requestPage.isClosed()) {
+        await this.requestPage.close();
       }
-      this.authPage = null;
+      this.requestPage = null;
 
       if (this.outputStream && !this.outputStream.writableEnded) {
         this.outputStream.end();
@@ -286,12 +292,25 @@ class Scraper {
     url: string,
     // path to output directory
     outputPath: string = process.cwd(),
-    // name of json file
+    // name of output json file
     fileName = 'adverts.json',
+    // use or don't use an authentication cookie to get phone numbers
+    // default value false means not to use an authentication cookie
+    // Authentication could be needed to get a phone number
+    // Whether you can get a phone number without authentication or
+    // not depends on the settings of the phone number owner
+    // So in order to obtain all phone numbers you have to log in to avito
+    // ATTENTION:
+    // But if you scrape phone numbers while logged in,
+    // your avito account will be banned after about 20 phone numbers
+    // So this code is written for educational purposes only and I don't
+    // recommend using it in real scraping
+    auth = false,
     // number of pages to scrape (default value 0 means to scrape all pages)
     pages = 0
   ): Promise<void> {
     try {
+      this.beforeStart(auth);
       await this.startBrowser();
       if (this.browser) {
         await this.scrapeAdverts(url, outputPath, fileName, pages);
