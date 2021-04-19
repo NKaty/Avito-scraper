c1910fe935221844056a6780d4511a70f8e38271
NKaty <nd.katy@gmail.com>
2021-04-19 18:10:21 +0300

    refactor logic of getting pages with adverts

--- src/scraper.ts
+++ src/scraper.ts
@@ -13,6 +13,7 @@ class Scraper {
   private browser: Nullable<Browser> = null;
   private outputStream: Nullable<Writable> = null;
   private requestPage: Nullable<Page> = null;
+  private advertsPage: Nullable<Page> = null;
   private firstAdvert = true;
   private auth = false;
   // To make pauses between requests
@@ -22,10 +23,11 @@ class Scraper {
   private userAgent =
     'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36';
 
-  private beforeStart(auth: boolean) {
+  private beforeStart(auth: boolean): void {
     this.browser = null;
     this.outputStream = null;
     this.requestPage = null;
+    this.advertsPage = null;
     this.firstAdvert = true;
     this.auth = auth;
   }
@@ -46,32 +48,31 @@ class Scraper {
   private async *getPages(url: string) {
     let ids: string[] = [];
     if (!this.browser) return ids;
-    let page: Nullable<Page> = await this.browser.newPage();
+    this.advertsPage = await this.browser.newPage();
     // Increase the maximum navigation time
     // Sometimes 30 seconds is not enough
-    await page.setDefaultTimeout(120000);
+    await this.advertsPage.setDefaultTimeout(120000);
 
     try {
-      await page.goto(url);
-      while (page) {
-        console.log(`Navigating to ${page.url()}...`);
-        await page.waitForSelector('div[class*="items-items-"]');
+      await this.advertsPage.goto(url);
+      while (this.advertsPage) {
+        console.log(`Navigating to ${this.advertsPage.url()}...`);
+        await this.advertsPage.waitForSelector('div[class*="items-items-"]');
         // Get only the ids of the adverts that are related to the url
         // (not vip, not other regions)
-        ids = await page.$$eval(
+        ids = await this.advertsPage.$$eval(
           'div[data-marker="catalog-serp"] > div[data-marker="item"] div[class*="iva-item-body-"] a[itemprop="url"]',
           links =>
             links.map(
               el => (el as HTMLAnchorElement).href.split('_').slice(-1)[0]
             )
         );
-        const result: [string[], Page] = [ids, page];
-        yield result;
+        yield ids;
 
         let nextButtonClasses: Nullable<string> = null;
         try {
-          nextButtonClasses = await page.$eval(
-            '[data-marker="pagination-button/next"]',
+          nextButtonClasses = await this.advertsPage.$eval(
+            'span[data-marker="pagination-button/next"]',
             item => item.classList.value
           );
         } catch (err) {}
@@ -80,8 +81,10 @@ class Scraper {
           !nextButtonClasses ||
           nextButtonClasses.includes('pagination-item_readonly')
         ) {
-          if (page && !page.isClosed()) await page.close();
-          page = null;
+          if (this.advertsPage && !this.advertsPage.isClosed()) {
+            await this.advertsPage.close();
+          }
+          this.advertsPage = null;
           console.log('last page');
         } else {
           console.log('click');
@@ -91,12 +94,16 @@ class Scraper {
           //   page.waitForNavigation(),
           //   page.click('span[data-marker="pagination-button/next"]'),
           // ]);
-          await page.click('span[data-marker="pagination-button/next"]');
+          await this.advertsPage.click(
+            'span[data-marker="pagination-button/next"]'
+          );
         }
       }
     } catch (err) {
-      console.log(`Could not get page ${page?.url()}: `, err);
-      if (page && !page.isClosed()) await page.close();
+      console.log(`Could not get page ${this.advertsPage?.url()}: `, err);
+      if (this.advertsPage && !this.advertsPage.isClosed()) {
+        await this.advertsPage.close();
+      }
     }
   }
 
@@ -121,12 +128,12 @@ class Scraper {
       await this.requestPage.setDefaultTimeout(120000);
       // For a mobile version
       await this.requestPage.setUserAgent(this.userAgent);
-      await this.requestPage.goto('https://m.avito.ru/#login');
-      await this.requestPage.waitForSelector('#app');
 
       // Username and password are stored in environment variables (.env file)
       // Remember: your avito account will be banned after 20 phone numbers
       if (this.auth && process.env.USERNAME && process.env.PASSWORD) {
+        await this.requestPage.goto('https://m.avito.ru/#login');
+        await this.requestPage.waitForSelector('#app');
         // Puppeteer recommends this pattern for click, but sometimes
         // it doesn't work (TimeoutError: Navigation timeout exceeded)
         // await Promise.all([
@@ -151,7 +158,7 @@ class Scraper {
         await this.requestPage.click('input[type="submit"');
       }
     } catch (err) {
-      console.log('Could not get cookie: ', err);
+      console.log('Could not get a request page: ', err);
     }
   }
 
@@ -184,7 +191,7 @@ class Scraper {
         );
         const phoneData = await phoneResponse.json();
         const phone = phoneData.result?.action?.uri?.split('%2B').slice(-1)[0];
-        console.log(phoneData);
+        // console.log(phoneData);
 
         advert = {
           title: advertData.title || '',
@@ -242,15 +249,13 @@ class Scraper {
     pages: number
   ): Promise<void> {
     let count = 0;
-    let page: Nullable<Page> = null;
 
     try {
       this.outputStream = createWriteStream(resolve(outputPath, fileName));
       this.outputStream.write('[');
 
-      for await (const [ids, currentPage] of this.getPages(url)) {
+      for await (const ids of this.getPages(url)) {
         if (ids.length) await this.getAdverts(ids);
-        page = currentPage;
         count++;
         console.log(count, ids.length);
         if (count === pages) break;
@@ -259,7 +264,9 @@ class Scraper {
       console.log('Got an error while scraping: ', err);
     }
 
-    if (page && !page.isClosed()) await page.close();
+    if (this.advertsPage && !this.advertsPage.isClosed()) {
+      await this.advertsPage.close();
+    }
     if (this.outputStream) this.outputStream.end('\n]\n');
   }
 
@@ -269,6 +276,11 @@ class Scraper {
     this.auth = false;
 
     try {
+      if (this.advertsPage && !this.advertsPage.isClosed()) {
+        await this.advertsPage.close();
+      }
+      this.advertsPage = null;
+
       if (this.requestPage && !this.requestPage.isClosed()) {
         await this.requestPage.close();
       }
